75a4a7df9524e6055e1763d2bf31ebbb577c5be7
Frikk Herding <frikksol@gmail.com>
2022-06-20 23:19:48 +0200

    ♻️ CORS headers

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@
 
 #[cfg(test)] mod tests;
 
-use rocket::{State, Shutdown};
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};use rocket::{State, Shutdown};
 use rocket::fs::{relative, FileServer};
 use rocket::form::Form;
 use rocket::response::stream::{EventStream, Event};
@@ -13,6 +15,7 @@ use rocket::tokio::select;
 #[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
 #[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
 #[serde(crate = "rocket::serde")]
+
 struct Message {
     #[field(validate = len(..30))]
     pub room: String,
@@ -42,6 +45,25 @@ async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStrea
     }
 }
 
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 /// Receive a message from a form submission and broadcast it to any receivers.
 #[post("/message", data = "<form>")]
 fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
@@ -53,6 +75,7 @@ fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
 fn rocket() -> _ {
     rocket::build()
         .manage(channel::<Message>(1024).0)
+        .attach(CORS)
         .mount("/", routes![post, events])
-        .mount("/", FileServer::from(relative!("static")))
+        //.mount("/", FileServer::from(relative!("build"))) // TODO Verify if I can host the app from rocket or if I need addition hosting
 }
