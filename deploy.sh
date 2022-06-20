ca35d48cf275688a5dd96775c546942e6bdf26c3
Frikk Herding <frikksol@gmail.com>
2022-06-20 23:20:03 +0200

    🚀 Deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 gcloud config set account frikksol@gmail.com
 gcloud config set project chat-app-353012
 
-gcloud builds submit --tag gcr.io/chat-app-353012/chat-app
-gcloud run deploy --image gcr.io/chat-app-353012/chat-app:latest --platform managed
+gcloud builds submit --timeout="15m" --tag gcr.io/chat-app-353012/chat-app
+gcloud run deploy chat-app --image gcr.io/chat-app-353012/chat-app:latest --platform managed
